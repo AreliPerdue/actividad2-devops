12bdec3bd50c62ff7432c88e9f3a81b727ad0bd3
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 08:47:19 +0000

    feat: subir backup a S3

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -26,3 +26,13 @@ ARCHIVO="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 tar -czf $ARCHIVO $DIRECTORIO
 
 echo "Archivo comprimido: $ARCHIVO"
+
+aws s3 cp $ARCHIVO s3://$BUCKET/
+
+echo "Archivo subido a S3"
+
+
+LOG="backup.log"
+
+echo "$(date) - Backup creado: $ARCHIVO" >> $LOG
+echo "$(date) - Subido a bucket: $BUCKET" >> $LOG
