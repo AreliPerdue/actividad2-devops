4a15555c665fef01bd6e4c2f9400c3b6bac52dd4
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 09:14:59 +0000

    feat: integrar script EC2 en deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,3 +9,8 @@ echo "Acción: $ACCION"
 echo "Instance ID: $INSTANCE_ID"
 echo "Directorio: $DIRECTORIO"
 echo "Bucket: $BUCKET"
+
+echo "Ejecutando script EC2..."
+
+python3 ec2/gestionar_ec2.py $ACCION $INSTANCE_ID
+
