154cf98bd5cb76ab24235107597d358059c285e4
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 05:24:30 +0000

    feat: detener instancia EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -10,9 +10,13 @@ def listar_instancias():
 
 if __name__ == "__main__":
     import sys
-
-if len(sys.argv) > 2 and sys.argv[1] == "start":
-    iniciar_instancia(sys.argv[2])
+if len(sys.argv) > 2:
+    if sys.argv[1] == "start":
+        iniciar_instancia(sys.argv[2])
+    elif sys.argv[1] == "stop":
+        detener_instancia(sys.argv[2])
+    else:
+        listar_instancias()
 else:
     listar_instancias()
 
@@ -20,3 +24,7 @@ def iniciar_instancia(instance_id):
     ec2 = boto3.client('ec2')
     ec2.start_instances(InstanceIds=[instance_id])
     print(f"Iniciando instancia {instance_id}")
+def detener_instancia(instance_id):
+    ec2 = boto3.client('ec2')
+    ec2.stop_instances(InstanceIds=[instance_id])
+    print(f"Deteniendo instancia {instance_id}")
