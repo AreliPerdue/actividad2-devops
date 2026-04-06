840803cdc72108e4ad38ce75ddf16a54fd209e71
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 06:35:09 +0000

    feat: usar bucket real en deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ echo "Iniciando despliegue..."
 python3 ec2/gestionar_ec2.py list
 
 # Ejecutar backup (cambia esto después con tu bucket)
-bash s3/backup_s3.sh . mi-bucket-devops
+bash s3/backup_s3.sh . al03072223
 
 echo "Despliegue finalizado"
