17ab52a46ef551e9d21e8bad28f6ba074d672ba9
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 04:13:28 +0000

    feat: iniciar instancia EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -9,4 +9,14 @@ def listar_instancias():
             print(f"ID: {instance['InstanceId']} | Estado: {instance['State']['Name']}")
 
 if __name__ == "__main__":
+    import sys
+
+if len(sys.argv) > 2 and sys.argv[1] == "start":
+    iniciar_instancia(sys.argv[2])
+else:
     listar_instancias()
+
+def iniciar_instancia(instance_id):
+    ec2 = boto3.client('ec2')
+    ec2.start_instances(InstanceIds=[instance_id])
+    print(f"Iniciando instancia {instance_id}")
