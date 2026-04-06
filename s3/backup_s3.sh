fa8c8c229780c539dc4eead9dc9e258bd2adda61
Areli Perdue <areli.perdue@gmail.com>
2026-04-06 06:16:20 +0000

    feat: compresion de archivos para backup

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -5,3 +5,9 @@ BUCKET=$2
 
 echo "Directorio: $DIRECTORIO"
 echo "Bucket: $BUCKET"
+
+ARCHIVO="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+tar -czf $ARCHIVO $DIRECTORIO
+
+echo "Archivo comprimido: $ARCHIVO"
